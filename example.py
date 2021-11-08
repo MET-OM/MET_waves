e10a34e4eb3e5f6e170f185a3796b2a1d97a0227
KonstantinChri <67804784+KonstantinChri@users.noreply.github.com>
2021-11-08 10:20:28 +0100

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,8 @@ from nora3_plt import plot_panarctic_map_timestep, plot_panarctic_map_mean
 
 # plot timesteps
 plot_panarctic_map_timestep(start_time =  '2005-01-07T18',end_time = '2005-01-09T18',variable ='hs')
+#terminal command for gif: convert -delay 20 -loop 0 *png hs.gif
+
 
 # plot mean
 #plot_panarctic_map_mean(start_time =  '2005-01-07T18',end_time = '2005-01-09T18',variable ='hs')
