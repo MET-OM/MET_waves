c127fec55e93fcbf3b9c67d5c401a5b75fbcc2e2
KonstantinChri <67804784+KonstantinChri@users.noreply.github.com>
2021-11-08 10:12:19 +0100

    Update nora3_plt.py

--- nora3_plt.py
+++ nora3_plt.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Nov  8 08:58:23 2021
 
-@author: KonstantinosChr
+@author: KonstantinChri
 """
 
 import matplotlib.pyplot as plt
@@ -13,17 +13,17 @@ import ecco_v4_py as ecco
 
 
 
-def plot_panarctic_map_timestep(start_time,end_time,variable, min_value, max_value):
+def plot_panarctic_map_timestep(start_time,end_time,variable):
     # The function plots/saves in png-format a given variable for all timesteps between start_time and end_time
     # start_time = start date for plotting e.g., '2005-01-07T18'
     # end_time   = end date for plotting e.g., '2005-01-09T18'
-    # min_value = mininum variable value
-    # max_value = maximum variable value 
     # Overview of the NORAE3 wave variables is given in: 
     # https://thredds.met.no/thredds/dodsC/windsurfer/mywavewam3km_agg/wam3kmhindcastaggregated.ncml.html
     
     url = 'https://thredds.met.no/thredds/dodsC/windsurfer/mywavewam3km_agg/wam3kmhindcastaggregated.ncml'
     ds = xr.open_dataset(url).sel(time=slice(start_time, end_time))
+    min_value = ds[variable].min()
+    max_value = ds[variable].max()
     for i in range(len(ds.time)):
         print(ds.time.values[i])
         fig, ax = plt.subplots()
@@ -41,3 +41,27 @@ def plot_panarctic_map_timestep(start_time,end_time,variable, min_value, max_val
         plt.close()
 
 
+def plot_panarctic_map_mean(start_time,end_time,variable):
+    # The function plots/saves in png-format the mean of a given variable between start_time and end_time
+    # start_time = start date for plotting e.g., '2005-01-07T18'
+    # end_time   = end date for plotting e.g., '2005-01-09T18'
+    # Overview of the NORAE3 wave variables is given in: 
+    # https://thredds.met.no/thredds/dodsC/windsurfer/mywavewam3km_agg/wam3kmhindcastaggregated.ncml.html
+    
+    url = 'https://thredds.met.no/thredds/dodsC/windsurfer/mywavewam3km_agg/wam3kmhindcastaggregated.ncml'
+    ds = xr.open_dataset(url).sel(time=slice(start_time, end_time))
+    min_value = ds[variable].min()
+    max_value = ds[variable].max()
+    fig, ax = plt.subplots()
+    plt.axes(projection=ccrs.NorthPolarStereo(true_scale_latitude=70))
+    ecco.plot_proj_to_latlon_grid(ds.longitude, ds.latitude, \
+                                 ds[variable].mean('time'), \
+                                 projection_type='stereo',\
+                                 plot_type = 'contourf', \
+                                 show_colorbar=True,
+                                 cmap = 'jet',
+                                 dx=1, dy=1,cmin=min_value, cmax=max_value,\
+                                 lat_lim=50);    
+    plt.title('Mean:'+start_time +'-' +end_time)
+    plt.savefig(variable+'avg'+start_time +'-' +end_time+'.png',bbox_inches = 'tight')
+    plt.close()
