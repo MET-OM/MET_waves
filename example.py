5638c3502bcc7f7add7a41865b4a3c12f9ac80ea
KonstantinChri <67804784+KonstantinChri@users.noreply.github.com>
2021-11-08 10:11:31 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
-from nora3_plt import plot_panarctic_map_timestep
+from nora3_plt import plot_panarctic_map_timestep, plot_panarctic_map_mean 
 
 # plot timesteps
-plot_panarctic_map_timestep(start_time =  '2005-01-07T18',end_time = '2005-01-09T18',variable ='hs' , min_value =0 , max_value=14)
+plot_panarctic_map_timestep(start_time =  '2005-01-07T18',end_time = '2005-01-09T18',variable ='hs')
+
+# plot mean
+#plot_panarctic_map_mean(start_time =  '2005-01-07T18',end_time = '2005-01-09T18',variable ='hs')
