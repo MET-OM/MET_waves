bc4d6125800cc1cf05fcf3bd312a75487ff30170
KonstantinChri <konstantinosc@met.no>
2021-11-09 07:17:48 +0100

    -

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 from met_waves import plot_panarctic_map
 
-plot_panarctic_map(start_time='2020-11-08T23', end_time='2020-11-11T00',
-                   product='WAM4C47', variable='hs', method='timestep')
+plot_panarctic_map(start_time='2020-11-08T23', end_time='2020-11-12T00',
+                   product='NORA3', variable='hs', method='timestep')
