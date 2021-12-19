e8448f4562ff0a3bc008279ffd90b2eb9de57cfb
KonstantinChri <konstantinosc@met.no>
2021-12-19 22:47:16 +0100

    ad example for Nora3

---
+++ example_plot_nora3.py
@@ -0,0 +1,5 @@
+import met_waves
+
+met_waves.plot_panarctic_map(start_time='1993-01-31T12:00', end_time='1993-02-04T12:00',
+                             product='NORA3', variable='hs', method='timestep')
+
