458fc46a7c50514e0326ce7c9ae72bc5b18ad457
KonstantinChri <kchrist87@gmail.com>
2022-04-22 22:56:04 +0200

    udate example

---
+++ example_ts_nora3.py
@@ -0,0 +1,5 @@
+import met_waves
+
+
+met_waves.plot_timeseries(start_time='1993-01-31T10:00', end_time='1993-01-31T23:00', lon=4.93,
+                lat=62.17, product='NORA3', variable='hs', write_csv=True, ts_obs=None)
